4366c08c222777d61f7cc4e8157cf8904b513066
refusta <refusta@refusta-pc>
2012-06-17 01:22:12 +0200

    Removed db local configuration Created SetMoza Added license Added GetMoza with country_codes

--- src/main/java/abelymiguel/miralaprima/SetMoza.java
+++ src/main/java/abelymiguel/miralaprima/SetMoza.java
@@ -1,26 +1,25 @@
 /*
- =============
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU Lesser General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+=============
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU Lesser General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package abelymiguel.miralaprima;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URISyntaxException;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,14 +27,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
  *
  * @author refusta
  */
-public class GetMoza extends HttpServlet {
+public class SetMoza extends HttpServlet {
 
     /**
      * Processes requests for both HTTP
@@ -47,7 +45,8 @@ public class GetMoza extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = null;
         try {
@@ -57,12 +56,21 @@ public class GetMoza extends HttpServlet {
         }
         response.setContentType("text/javascript;charset=UTF-8");
 
-        String country_code = null;
+        String url_moza;
+        url_moza = request.getParameter("url_moza");
+
+        String country_code;
         country_code = request.getParameter("country_code");
 
+        if (country_code == null) {
+            country_code = "ALL";
+        }
+
+        JSONObject jsonObject;
         String json_str;
-        JSONArray jsonArray = new JSONArray(this.searchInDB(country_code));
-        json_str = jsonArray.toString();
+
+        jsonObject = new JSONObject(this.setInDB(url_moza, country_code));
+        json_str = jsonObject.toString();
 
         String jsonpCallback = request.getParameter("callback");
         if (jsonpCallback != null) {
@@ -114,41 +122,36 @@ public class GetMoza extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
-    private ArrayList<HashMap<String, Object>> searchInDB(String country_code) {
+    private HashMap<String, String> setInDB(String url_moza, String country_code) {
 
-        ArrayList<HashMap<String, Object>> respuestaJson = new ArrayList<HashMap<String, Object>>();
+        HashMap<String, String> respuestaJson = new HashMap<String, String>();
         try {
-            
-            ResultSet rs;
-
-            if (country_code == null) {
-                rs = Utils.getConnection().createStatement().executeQuery("SELECT url_prima, provider FROM photos WHERE approved = 1");
-
-                while (rs.next()) {
-                    HashMap<String, Object> objetoJson = new HashMap<String, Object>();
-                    String url = rs.getString("url_prima");
-                    String provider = rs.getString("provider");
-                    objetoJson.put(url, provider);
-                    respuestaJson.add(objetoJson);
-                }
+            Timestamp date_added = this.getTimestamp();
+            String provider;
+            if (url_moza.contains("http://")){
+                provider = url_moza.replace("http://", "");
+                provider = "http://" + provider.substring(0, provider.indexOf("/"));
             } else {
-                rs = Utils.getConnection().createStatement().executeQuery("SELECT url_prima, provider FROM photos WHERE approved = 1 AND country_code='" + country_code + "'");
-
-                while (rs.next()) {
-                    HashMap<String, Object> objetoJson = new HashMap<String, Object>();
-                    String url = rs.getString("url_prima");
-                    String provider = rs.getString("provider");
-                    objetoJson.put(url, provider);
-                    respuestaJson.add(objetoJson);
-                }
+                provider = "Provider not found";
             }
-
-            rs.close();
+            Utils.getConnection().createStatement().execute("INSERT INTO `photos` (`url_prima`, `provider`, `approved`, `country_code`, `date_added`) VALUES ('" + url_moza + "', '" + provider + "', 0, '" + country_code + "', '" + date_added + "')");
+            respuestaJson.put("result", "OK");
         } catch (SQLException ex) {
             Logger.getLogger(GetMoza.class.getName()).log(Level.SEVERE, null, ex);
+            respuestaJson.put("result", "ERROR");
         } catch (URISyntaxException ex) {
             Logger.getLogger(GetMoza.class.getName()).log(Level.SEVERE, null, ex);
+            respuestaJson.put("result", "ERROR");
         }
         return respuestaJson;
+    }
+
+    private Timestamp getTimestamp() {
+
+        Timestamp date_added = null;
+        java.util.Date date = new java.util.Date();
+        date_added = new Timestamp(date.getTime());
+
+        return date_added;
     }
 }
